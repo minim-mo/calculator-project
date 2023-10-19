7c372527f433c46982302506fd837a3f73343a9b
minim-mo <147269904+minim-mo@users.noreply.github.com>
2023-10-19 11:48:19 -0400

    add id to clear button

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 console.log("hello!")
 
+// create addEventListener()
+
+document.addEventListener("DOMContentLoaded", function() {
+   //store all components on HTML to JS 
+    let clear = document.querySelector("#clear-btn");
+    let equal = document.querySelector(".equal");
+    let decimal = document.querySelector(".decimal");
+
+    let numbers = document.querySelectorAll(".numbers");
+    let operators = document.querySelectorAll(".operators");
+
+    let previousScreen = document.querySelector(".previous");
+    let currentScreen = document.querySelector(".current");
+});
